dd699cbc83ccfce8bc46f169a4a3cb5cbada82be
Niicck <niicck@users.noreply.github.com>
2022-12-12 22:59:55 -0500

    serve static files locally

--- docker/scripts/docker-start.sh
+++ docker/scripts/docker-start.sh
@@ -6,5 +6,10 @@ CURRENT_DIR=`dirname ${BASH_SOURCE[0]}`
 ROOT_DIR=$CURRENT_DIR/../..
 
 python $ROOT_DIR/manage.py migrate
-# gunicorn --workers=1 --bind=${DJANGO_HOST}:${DJANGO_PORT} --chdir=$ROOT_DIR niicck_django_cookiecutter.wsgi:application
-python $ROOT_DIR/manage.py runserver ${DJANGO_HOST}:${DJANGO_PORT}
+
+if [ $DJANGO_CONFIGURATION == "Local" ]; then
+    python $ROOT_DIR/manage.py runserver ${DJANGO_HOST}:${DJANGO_PORT} --insecure
+else
+    echo "TODO"
+    # gunicorn --workers=1 --bind=${DJANGO_HOST}:${DJANGO_PORT} --chdir=$ROOT_DIR niicck_django_cookiecutter.wsgi:application
+fi
--- niicck_django_cookiecutter/settings.py
+++ niicck_django_cookiecutter/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 from configurations import Configuration
@@ -17,8 +18,10 @@ from decouple import config
 
 
 class Base(Configuration):
+    DJANGO_CONFIGURATION = config("DJANGO_CONFIGURATION")
+
     # Build paths inside the project like this: BASE_DIR / 'subdir'.
-    BASE_DIR = Path(__file__).resolve().parent.parent
+    BASE_DIR = Path(__file__).resolve().parent
 
     ROOT_URLCONF = "niicck_django_cookiecutter.urls"
 
@@ -86,7 +89,10 @@ class Base(Configuration):
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-    STATIC_URL = "static/"
+    STATIC_URL = "/static/"
+    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    MEDIA_URL = "/media/"
+    MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
     # Default primary key field type
     # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
