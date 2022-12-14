c0767deb9e397e7a50f745a6344c79d24b90da56
Niicck <niicck@users.noreply.github.com>
2022-12-14 12:14:24 -0500

    allow migrations_check to access db

--- utils/pre_commit/migrations_check.sh
+++ utils/pre_commit/migrations_check.sh
@@ -3,6 +3,14 @@
 # Check that there aren't any unapplied model migrations.
 
 CURRENT_DIR=`dirname ${BASH_SOURCE[0]}`
+
+# Source ENV_FILE if it exists
+ENV_FILE=$CURRENT_DIR/../../.env
+[ -e "$ENV_FILE" ] && . "$ENV_FILE"
+
 export DJANGO_CONFIGURATION=Test
+# Connect to POSTGRES_HOST=localhost when accessing postgres from outside of docker
+# Connect to POSTGRES_HOST=postgres, when accessing postgres from within docker
+export POSTGRES_HOST=localhost
 
 poetry run python $CURRENT_DIR/../../manage.py makemigrations --check --dry-run
