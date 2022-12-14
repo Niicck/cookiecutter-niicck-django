b8c8bb51f35cd634964d61f99e2a26351e4d3abd
Niicck <niicck@users.noreply.github.com>
2022-12-14 12:21:56 -0500

    use argon2 password hashing

--- cookiecutter_niicck_django/settings.py
+++ cookiecutter_niicck_django/settings.py
@@ -61,6 +61,16 @@ class Base(Configuration):
         },
     ]
 
+    # https://docs.djangoproject.com/en/dev/topics/auth/passwords/#using-argon2-with-django
+
+    PASSWORD_HASHERS = [
+        "django.contrib.auth.hashers.Argon2PasswordHasher",
+        "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+        "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+        "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+        "django.contrib.auth.hashers.ScryptPasswordHasher",
+    ]
+
     # Internationalization
     # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
